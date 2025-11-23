2012e1cca9882baf3cb0452f6a298f8781706499
DannaSarai <dannamo2005@gmail.com>
2025-11-23 17:36:58 -0500

    Assets y config

--- collectomarket-project/config/db.php
+++ collectomarket-project/config/db.php
@@ -1,8 +1,8 @@
 <?php
 // Credenciales de la Base de Datos
 $servername = "localhost";
-$username = "root"; // Usuario por defecto de XAMPP/MariaDB
-$password = "";     // Contraseña por defecto de XAMPP (suele ser vacía)
+$username = "root"; // Usuario por defecto de XAMPP
+$password = "";     // Contraseña por defecto de XAMPP
 $dbname = "collectomart_db"; // Nombre de la base de datos
 
 // Intentar la conexión
@@ -16,6 +16,4 @@ if ($conn->connect_error) {
 // Opcional: Establecer juego de caracteres a UTF8
 $conn->set_charset("utf8");
 
-// NOTA: Para usar este archivo en cualquier página, solo necesitas:
-// require_once 'config/db.php';
 ?>
